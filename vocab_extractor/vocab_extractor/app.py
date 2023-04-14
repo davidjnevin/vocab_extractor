f2731fedc778e233a623d2cc628c0b7019379a34
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-04-15 00:17:41 +0200

    Refactor to use python-docx

--- vocab_extractor/vocab_extractor/app.py
+++ vocab_extractor/vocab_extractor/app.py
@@ -1,57 +1,70 @@
 import glob
 import re
 import sqlite3
-from typing import List, Set
+from typing import Set
+
+from docx import Document
+
+def extract_vocabulary(text: str) -> Set[str]:
+    words = set(re.findall(r"\b\w+\b", text))
+    return words
 
 
-def extract_vocabulary(file_content: str) -> Set[str]:
-    """Extract unique vocabulary words from a given file content."""
-    pattern = re.compile(r"\b\w+\b")
-    return set(pattern.findall(file_content))
+def read_file(file_path: str) -> str:
+    doc = Document(file_path)
+    content = "\n".join([para.text for para in doc.paragraphs])
+    return content
 
 
 def create_database(db_name: str) -> sqlite3.Connection:
-    """Create a SQLite database and vocabulary table."""
     conn = sqlite3.connect(db_name)
-    c = conn.cursor()
-    c.execute(
-        """
-    CREATE TABLE IF NOT EXISTS vocabulary (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        word TEXT UNIQUE
-    )"""
+    conn.execute(
+        """CREATE TABLE IF NOT EXISTS vocabulary (
+                        id INTEGER PRIMARY KEY,
+                        word TEXT UNIQUE
+                    )"""
     )
     conn.commit()
     return conn
 
 
-def insert_word(conn: sqlite3.Connection, word: str) -> None:
-    """Insert a unique vocabulary word into the database."""
-    c = conn.cursor()
+def insert_word(word: str, conn: sqlite3.Connection) -> None:
     try:
-        c.execute("INSERT OR IGNORE INTO vocabulary (word) VALUES (?)", (word,))
+        conn.execute("INSERT INTO vocabulary (word) VALUES (?)", (word,))
         conn.commit()
-    except sqlite3.Error as e:
-        print(f"Error inserting word '{word}': {e}")
+    except sqlite3.IntegrityError:
+        pass
 
 
-def process_reports(report_files: List[str], conn: sqlite3.Connection) -> None:
-    """Process the report files and extract unique vocabulary words."""
-    for report_file in report_files:
-        with open(report_file, "r", encoding="utf-8") as file:
-            content = file.read()
-            vocab_words = extract_vocabulary(content)
+def process_reports(report_files: list[str], conn: sqlite3.Connection) -> None:
+    for file_path in report_files:
+        print(f"Processing file: {file_path}")
+        content = read_file(file_path)
+        vocabulary = extract_vocabulary(content)
 
-            for word in vocab_words:
-                insert_word(conn, word)
+        for word in vocabulary:
+            insert_word(word, conn)
 
 
-def main(max_files: int = 10) -> None:
-    db_name = "vocabulary.db"
+def batch_files(report_files: list[str], batch_size: int = 100) -> list[str]:
+    for i in range(0, len(report_files), batch_size):
+        yield report_files[i : i + batch_size]
+
+
+def process_all_reports(report_files: list[str], conn: sqlite3.Connection, batch_size: int = 100) -> None:
+    for idx, batch in enumerate(batch_files(report_files, batch_size), start=1):
+        process_reports(batch, conn)
+        print(f"Processed batch {idx}")
+
+
+def main() -> None:
+    db_name = "vocabulary_2.db"
     conn = create_database(db_name)
-    report_files = glob.glob('../reports/**/*.[dD][oO][cC]*', recursive=True)
-    report_files = report_files[:max_files]  # Limit number of files to process
-    process_reports(report_files, conn)
+    doc_files = glob.glob('./reports/**/*.doc', recursive=True)
+    docx_files = glob.glob('./reports/**/*.docx', recursive=True)
+    report_files = doc_files + docx_files
+    # breakpoint()
+    process_all_reports(report_files, conn)
     conn.close()
 
 
