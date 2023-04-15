292be15aef10e7c77b3cec916f574701a3b405f7
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-04-15 15:15:52 +0200

    Refactor for tokenization methods

--- vocab_extractor/vocab_extractor/app.py
+++ vocab_extractor/vocab_extractor/app.py
@@ -1,25 +1,11 @@
 import glob
 import sqlite3
+
 from vocab_extractor.extract import extract_vocabulary
-from vocab_extractor.io_utils import read_file
+from vocab_extractor.io_utils import create_database, read_file, insert_word
 
-def create_database(db_name: str) -> sqlite3.Connection:
-    conn = sqlite3.connect(db_name)
-    conn.execute(
-        """CREATE TABLE IF NOT EXISTS vocabulary (
-                        id INTEGER PRIMARY KEY,
-                        word TEXT UNIQUE
-                    )"""
-    )
-    conn.commit()
-    return conn
+batch_size = 2
 
-def insert_word(word: str, conn: sqlite3.Connection) -> None:
-    try:
-        conn.execute("INSERT INTO vocabulary (word) VALUES (?)", (word,))
-        conn.commit()
-    except sqlite3.IntegrityError:
-        pass
 
 def process_reports(report_files: list[str], conn: sqlite3.Connection) -> None:
     for file_path in report_files:
@@ -27,27 +13,30 @@ def process_reports(report_files: list[str], conn: sqlite3.Connection) -> None:
         content = read_file(file_path)
         vocabulary = extract_vocabulary(content)
 
-        for word in vocabulary:
-            insert_word(word, conn)
+        for word, pos in vocabulary:
+            insert_word(word, pos, conn)
 
-def batch_files(report_files: list[str], batch_size: int = 100) -> list[str]:
+
+def batch_files(report_files: list[str], batch_size: int = 2) -> list[str]:
     for i in range(0, len(report_files), batch_size):
         yield report_files[i : i + batch_size]
 
-def process_all_reports(report_files: list[str], conn: sqlite3.Connection, batch_size: int = 100) -> None:
+
+def process_all_reports(report_files: list[str], conn: sqlite3.Connection, batch_size: int = 2) -> None:
     for idx, batch in enumerate(batch_files(report_files, batch_size), start=1):
         process_reports(batch, conn)
         print(f"Processed batch {idx}")
 
+
 def main() -> None:
-    db_name = "vocabulary_2.db"
+    db_name = "vocabulary_token_1.db"
     conn = create_database(db_name)
-    doc_files = glob.glob('./reports/**/*.doc', recursive=True)
-    docx_files = glob.glob('./reports/**/*.docx', recursive=True)
+    doc_files = glob.glob("./reports/**/*.doc", recursive=True)
+    docx_files = glob.glob("./reports/**/*.docx", recursive=True)
     report_files = doc_files + docx_files
     process_all_reports(report_files, conn)
     conn.close()
 
+
 if __name__ == "__main__":
     main()
-
--- vocab_extractor/vocab_extractor/io_utils.py
+++ vocab_extractor/vocab_extractor/io_utils.py
@@ -1,7 +1,31 @@
+import sqlite3
+
 from docx import Document
+
+
+def insert_word(word: str, pos: str, conn: sqlite3.Connection) -> None:
+    try:
+        conn.execute("INSERT INTO vocabulary (word, pos) VALUES (?, ?)", (word, pos))
+        conn.commit()
+    except sqlite3.IntegrityError as err:
+        print(f"Update Error - {err}")
+
+
+def create_database(db_name: str) -> sqlite3.Connection:
+    conn = sqlite3.connect(db_name)
+    conn.execute(
+        """CREATE TABLE IF NOT EXISTS vocabulary (
+                        id INTEGER PRIMARY KEY,
+                        word TEXT NOT NULL,
+                        pos TEXT NOT NULL,
+                        UNIQUE (word, pos)
+                    )"""
+    )
+    conn.commit()
+    return conn
+
 
 def read_file(file_path: str) -> str:
     doc = Document(file_path)
     content = "\n".join([para.text for para in doc.paragraphs])
     return content
-
