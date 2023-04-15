3ecdaed25631a480991a80d6b187a30623066a87
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-04-15 17:04:55 +0200

    Update test_insert_words

--- vocab_extractor/tests/test_io_utils.py
+++ vocab_extractor/tests/test_io_utils.py
@@ -2,7 +2,7 @@ import sqlite3
 import pytest
 from pathlib import Path
 
-from vocab_extractor.io_utils import create_database, insert_word
+from vocab_extractor.io_utils import create_database, insert_words
 
 
 def test_create_database(tmp_path: Path):
@@ -11,12 +11,16 @@ def test_create_database(tmp_path: Path):
     assert isinstance(conn, sqlite3.Connection)
 
 
-def test_insert_word(tmp_path: Path):
+def test_insert_words(tmp_path: Path):
     temp_db = tmp_path / "test.db"
     conn = create_database(str(temp_db))
-    word = "test"
-    pos = "NN"
-    insert_word(word, pos, conn)
-    cursor = conn.execute("SELECT word, pos FROM vocabulary WHERE word=? AND pos=?", (word, pos))
-    assert cursor.fetchone() == (word, pos)
+    words = [("test", "NN"), ("sample", "JJ")]
+    insert_words(words, conn)
 
+    cursor = conn.cursor()
+    cursor.execute("SELECT word, pos FROM vocabulary")
+    result = cursor.fetchall()
+
+    assert len(result) == 2
+    assert ("test", "NN") in result
+    assert ("sample", "JJ") in result
