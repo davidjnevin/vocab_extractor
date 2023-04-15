09aaba2d1fa64f581d41a169adb848b505cd13cb
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-04-15 19:21:17 +0200

    Clean db before use

--- vocab_extractor/vocab_extractor/io_utils.py
+++ vocab_extractor/vocab_extractor/io_utils.py
@@ -8,18 +8,25 @@ def insert_words(words: list[Tuple[str, str]], conn: sqlite3.Connection) -> None
     with conn:
         conn.executemany("INSERT OR IGNORE INTO vocabulary (word, pos) VALUES (?, ?)", words)
 
+
 def create_database(db_name: str) -> sqlite3.Connection:
     conn = sqlite3.connect(db_name)
+    conn.execute("DROP TABLE IF EXISTS vocabulary")
     conn.execute(
         """CREATE TABLE IF NOT EXISTS vocabulary (
                         id INTEGER PRIMARY KEY,
-                        word TEXT NOT NULL,
-                        pos TEXT NOT NULL,
-                        UNIQUE (word, pos)
+                        word TEXT,
+                        pos TEXT,
+                        UNIQUE(word, pos)
                     )"""
     )
     conn.commit()
     return conn
+
+
+def clear_vocabulary_table(conn: sqlite3.Connection) -> None:
+    with conn:
+        conn.execute("DELETE FROM vocabulary")
 
 
 def read_file(file_path: str) -> str:
