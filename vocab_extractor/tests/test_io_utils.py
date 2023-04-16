bba9b16b8864985994f621433a5b85a39a4207c8
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-04-16 18:59:29 +0200

    Add word_count to DB schema

--- vocab_extractor/tests/test_io_utils.py
+++ vocab_extractor/tests/test_io_utils.py
@@ -15,21 +15,21 @@ def test_create_database(tmp_path):
 def test_insert_words(tmp_path):
     db_file = tmp_path / "test.db"
     with create_database(db_file) as conn:
-        words = [("hello", "NN"), ("world", "NN"), ("test", "NN")]
+        words = [("hello", "NN", 1), ("world", "NN", 1), ("test", "NN", 1)]
         insert_words(words, conn)
         cur = conn.cursor()
-        cur.execute("SELECT word, pos FROM vocabulary")
+        cur.execute("SELECT word, pos, word_count FROM vocabulary")
         result = cur.fetchall()
-        assert set(result) == {("hello", "NN"), ("world", "NN"), ("test", "NN")}
+        assert set(result) == {("hello", "NN", 1), ("world", "NN", 1), ("test", "NN", 1)}
 
 
 def test_clear_vocabulary_table(tmp_path):
     db_file = tmp_path / "test.db"
     with create_database(db_file) as conn:
-        words = [("hello", "NN"), ("world", "NN"), ("test", "NN")]
+        words = [("hello", "NN", 1), ("world", "NN", 1), ("test", "NN", 2)]
         insert_words(words, conn)
         clear_vocabulary_table(conn)
         cur = conn.cursor()
-        cur.execute("SELECT word, pos FROM vocabulary")
+        cur.execute("SELECT word, pos, word_count FROM vocabulary")
         result = cur.fetchall()
         assert len(result) == 0
